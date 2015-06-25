fe1564ea50cce3e427fbdca39372a2cae2c0450a
12301079-Qianwenshu <12301079@bjtu.edu.cn>
2015-06-25 09:06:06 +0800

    AR360-0.22
    AR360-0.22

--- 代码/RELEASE/AR360/PageTest3D.cpp
+++ 代码/RELEASE/AR360/PageTest3D.cpp
@@ -41,24 +41,18 @@ public:
 
 void PageTest3D::onInit(void* pData){
 	
-	s_device()->setWindowCaption(L"Hello World! - Irrlicht Engine Demo");
-	s_guienv()->addStaticText(L"Hello World! This is the Irrlicht Software renderer!",
-		rect<s32>(10, 10, 260, 22), true);
 
-	IAnimatedMesh* mesh = s_smgr()->getMesh("media/sydney.md2");
+	IAnimatedMesh* mesh = s_smgr()->getMesh("Ar360Res/UI/sydney.md2");
 
 	IAnimatedMeshSceneNode* node = s_smgr()->addAnimatedMeshSceneNode(mesh);
 
-
-	if (node)
-	{
-		node->setMaterialFlag(EMF_LIGHTING, false);
-		node->setMD2Animation(scene::EMAT_STAND);
-		node->setMaterialTexture(0, s_driver()->getTexture("media/sydney.bmp"));
-	}
+	node->setMaterialFlag(EMF_LIGHTING, false);
+	node->setMD2Animation(scene::EMAT_STAND);
+	node->setMaterialTexture(0, s_driver()->getTexture("Ar360Res/UI/sydney.bmp"));
+	
 
 	s_smgr()->addCameraSceneNode(0, vector3df(0, 30, -40), vector3df(0, 5, 0));
-	//s_guienv()->loadGUI("PageTest.xml");
+
 	s_guienv()->addButton(rect<s32>(10, 240, 110, 240 + 32), 0, GUI_ID_BUTTON_BACK_TO_INDEX,
 		L"Back", L"description:Back");
 	s_device()->setEventReceiver(PageTest3DEventReceiver::Get());
